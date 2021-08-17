2654d5e62e2319a094e31f345d7b5c014368827d
Javier Melendrez <81501034+javiermelendrez1@users.noreply.github.com>
2021-08-16 17:35:58 -0700

    added Google Authentication OnClick

--- chatapp/src/components/Login/Login.js
+++ chatapp/src/components/Login/Login.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import {GoogleOutlined, FacebookOutlined} from '@ant-design/icons';
-
+import 'firebase/app';
+import { auth } from '../../firebase';
+import firebase from 'firebase/app';
 const Login = () => {
     return (
         <div id="login-page">
             <div id="login-card">
                 <h2>welcome to firebase chat</h2>
-                <div className="login-button google">
+                <div className="login-button google"
+                onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}
+                >
                     <GoogleOutlined/> Sign in With Google
                 </div>
                 <br />
                 <br />
-                <div className="login-button facebook">
-                    <FacebookOutlined/> Sign in With Facebook
-                </div>
             </div>
         </div>
     );
